3634a02c57d31fb66f95953e76a478cdc3566a18
quentintyr <quentin.tyr@gmail.com>
2026-05-11 14:22:06 +0200

    REM | required from the email fields in register to make it optional

--- application/view/register/index.php
+++ application/view/register/index.php
@@ -4,15 +4,15 @@
     <?php $this->renderFeedbackMessages(); ?>
 
     <!-- login box on left side -->
-    <div class="login-box" style="width: 100%; display: block;">
+    <div class="register-box-new" style="width: 50%; display: block;">
         <h2>Register a new account</h2>
 
         <!-- register form -->
         <form method="post" action="<?php echo Config::get('URL'); ?>register/register_action">
             <!-- the user name input field uses a HTML5 pattern check -->
             <input type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" placeholder="Username (letters/numbers, 2-64 chars)" required />
-            <input type="text" name="user_email" placeholder="email address (a real address)" required />
-            <input type="text" name="user_email_repeat" placeholder="repeat email address (to prevent typos)" required />
+            <input type="text" name="user_email" placeholder="email address (a real address)"  />
+            <input type="text" name="user_email_repeat" placeholder="repeat email address (to prevent typos)"  />
             <input type="password" name="user_password_new" pattern=".{6,}" placeholder="Password (6+ characters)" required autocomplete="off" />
             <input type="password" name="user_password_repeat" pattern=".{6,}" required placeholder="Repeat your password" autocomplete="off" />
 
