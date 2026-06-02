1c720f3bda88f20664f39a70ed91491f5bcf9a6c
quentintyr <quentin.tyr@gmail.com>
2026-06-02 20:22:44 +0200

    FEAT | simple html view for the messages inbox - chat window - open chats with users

--- application/view/message/showChat.php
+++ application/view/message/showChat.php
@@ -1,25 +1,32 @@
-
-
 <div class="container">
-
-    <h1>Chat with User</h1>
+    <h1>Chat with <?= $this->user->user_name; ?></h1>
+    
     <div class="box">
-
-        <!-- echo out the system feedback (error and success messages) -->
         <?php $this->renderFeedbackMessages(); ?>
+
+        <a href="<?php echo Config::get('URL'); ?>message/index">Back</a>
+        <br><br>
+
+        <div id="chat-window" style="height:300px; border:1px solid black; overflow:scroll;">
+        <?php if (!empty($this->conversation)) { ?>
+            <?php foreach ($this->conversation as $m) { ?>
+                <div>
+                    <b><?= ($m->sender_id == Session::get('user_id') ? 'You' : $this->user->user_name); ?>:</b>
+                    <?= htmlspecialchars($m->content); ?>
+                    <br>
+                    <small><?= $m->created_at; ?></small>
+                </div>
+                <hr>
+            <?php } ?>
+        <?php } else { ?>
+            <p>No messages yet.</p>
+        <?php } ?>
         </div>
-        <div>
-            <table id="users-table" class="overview-table">
-                <thead>
-                <tr>
-                    <td>Id</td>
-                    <td>Avatar</td>
-                    <td>Username</td>
-                    <td>Message</td>
-                </tr>
-                <br>
-                </thead>
-            </table>
-        </div>
+
+        <form method="post" action="<?php echo Config::get('URL'); ?>message/send/<?php echo $this->user->user_id; ?>">
+            <input type="text" name="text" />
+            <input type="submit" value="Send" />
+        </form>
+
     </div>
 </div>
