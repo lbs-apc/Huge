20d4cfd9fbb80875309dd1ca35fa1dc5bc46144f
quentintyr <quentin.tyr@gmail.com>
2026-06-24 07:53:49 +0200

    SQL | added default plugins script - 3 default plugins

---
+++ application/_installation/12-insert-default-plugins.sql
@@ -0,0 +1,4 @@
+INSERT INTO `plugins` (plugin_name, version, installed, active, `path`, created_at) VALUES
+('Darkmode', '1.0', 1, 0, 'application/plugins/darkmode', NOW()),
+('Image Optimizer', '1.0', 1, 0, 'application/plugins/image_optimizer', NOW()),
+('Watermarker', '1.0', 1, 0, 'application/plugins/watermarker', NOW());
