edba5f62c0bb77b82b9c05155e9fc6c118685970
quentintyr <quentin.tyr@gmail.com>
2026-06-30 10:32:44 +0200

    FEAT | new section for installed plugins - moved plugins to all plugins section - if plugin gets installed it moves from all plugins to installed   plugins

--- application/view/plugin/index.php
+++ application/view/plugin/index.php
@@ -3,8 +3,22 @@
 
     <?php $this->renderFeedbackMessages(); ?>
 
-    <div class="box">
-        <h3>Plugins</h3>
+    <?php
+    $installed = [];
+    $available = [];
+    if (!empty($this->plugins)) {
+        foreach ($this->plugins as $plugin) {
+            if ((int)$plugin->user_installed > 0) {
+                $installed[] = $plugin;
+            } else {
+                $available[] = $plugin;
+            }
+        }
+    }
+    ?>
+
+    <div class="box" style="margin-bottom: 30px;">
+        <h3>My Installed Plugins</h3>
 
         <table style="width:100%; border-collapse: collapse;">
             <thead>
@@ -16,7 +30,7 @@
                 </tr>
             </thead>
             <tbody>
-                <?php if (!empty($this->plugins)) { foreach ($this->plugins as $plugin) { ?>
+                <?php if (!empty($installed)) { foreach ($installed as $plugin) { ?>
                     <tr>
                         <td style="padding:8px; border-bottom:1px solid #eee;"><?php echo $this->encodeHTML($plugin->plugin_name); ?></td>
                         <td style="padding:8px; border-bottom:1px solid #eee;"><?php echo $this->encodeHTML($plugin->version); ?></td>
@@ -26,29 +40,57 @@
                             <small>Downloads: <?php echo (int)$plugin->total_installed; ?></small>
                         </td>
                         <td style="padding:8px; border-bottom:1px solid #eee;">
-                            <?php if ((int)$plugin->user_installed <= 0) { ?>
-                                <form action="<?php echo Config::get('URL') . 'plugin/install/' . $plugin->plugin_id; ?>" method="POST" class="plugin-action-form">
-                                    <button type="submit" class="btn-plugin btn-install">Install</button>
-                                </form>
-                            <?php } else { ?>
-                                <form action="<?php echo Config::get('URL') . 'plugin/toggle/' . $plugin->plugin_id; ?>" method="POST" class="plugin-action-form">
-                                    <?php if ($plugin->user_active == 1) { ?>
-                                        <button type="submit" class="btn-plugin btn-deactivate">Deactivate</button>
-                                    <?php } else { ?>
-                                        <button type="submit" class="btn-plugin btn-activate">Activate</button>
-                                    <?php } ?>
-                                </form>
-                                <form action="<?php echo Config::get('URL') . 'plugin/uninstall/' . $plugin->plugin_id; ?>" method="POST" class="plugin-action-form">
-                                    <button type="submit" class="btn-plugin btn-uninstall">Uninstall</button>
-                                </form>
-                            <?php } ?>
+                            <form action="<?php echo Config::get('URL') . 'plugin/toggle/' . $plugin->plugin_id; ?>" method="POST" class="plugin-action-form">
+                                <?php if ($plugin->user_active == 1) { ?>
+                                    <button type="submit" class="btn-plugin btn-deactivate">Deactivate</button>
+                                <?php } else { ?>
+                                    <button type="submit" class="btn-plugin btn-activate">Activate</button>
+                                <?php } ?>
+                            </form>
+                            <form action="<?php echo Config::get('URL') . 'plugin/uninstall/' . $plugin->plugin_id; ?>" method="POST" class="plugin-action-form">
+                                <button type="submit" class="btn-plugin btn-uninstall">Uninstall</button>
+                            </form>
                         </td>
                     </tr>
                 <?php } } else { ?>
-                    <tr><td colspan="4" style="padding:8px;">No plugins found.</td></tr>
+                    <tr><td colspan="4" style="padding:8px;">No installed plugins found.</td></tr>
                 <?php } ?>
             </tbody>
         </table>
+    </div>
 
+    <div class="box">
+        <h3>All Plugins</h3>
+
+        <table style="width:100%; border-collapse: collapse;">
+            <thead>
+                <tr>
+                    <th style="text-align:left; padding:8px; border-bottom:1px solid #ddd;">Name</th>
+                    <th style="text-align:left; padding:8px; border-bottom:1px solid #ddd;">Version</th>
+                    <th style="text-align:left; padding:8px; border-bottom:1px solid #ddd;">Status</th>
+                    <th style="text-align:left; padding:8px; border-bottom:1px solid #ddd;">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if (!empty($available)) { foreach ($available as $plugin) { ?>
+                    <tr>
+                        <td style="padding:8px; border-bottom:1px solid #eee;"><?php echo $this->encodeHTML($plugin->plugin_name); ?></td>
+                        <td style="padding:8px; border-bottom:1px solid #eee;"><?php echo $this->encodeHTML($plugin->version); ?></td>
+                        <td style="padding:8px; border-bottom:1px solid #eee;">
+                            Inactive
+                            <br/>
+                            <small>Downloads: <?php echo (int)$plugin->total_installed; ?></small>
+                        </td>
+                        <td style="padding:8px; border-bottom:1px solid #eee;">
+                            <form action="<?php echo Config::get('URL') . 'plugin/install/' . $plugin->plugin_id; ?>" method="POST" class="plugin-action-form">
+                                <button type="submit" class="btn-plugin btn-install">Install</button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php } } else { ?>
+                    <tr><td colspan="4" style="padding:8px;">No other plugins found.</td></tr>
+                <?php } ?>
+            </tbody>
+        </table>
     </div>
 </div>
